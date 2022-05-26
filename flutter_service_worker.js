65366935b982c2082d5340de99c07863f248f95e
dimstr <dim_str@vk.com>
2022-05-26 18:30:24 +0300

    New version

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -26,9 +26,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "5138ec9bd53b8e3684944919a972addf",
-"/": "5138ec9bd53b8e3684944919a972addf",
-"main.dart.js": "252de0815022b4256349e1067560fb54",
+"index.html": "be0a1a2d7da062d19b3a1a3b1c36be47",
+"/": "be0a1a2d7da062d19b3a1a3b1c36be47",
+"main.dart.js": "0fb475adeecf94a8ebba27f82f5573e7",
 "manifest.json": "2ffbf35b0de3481504fdd46c95a53fdb",
 "version.json": "5b2eb9a1c169949d4769f93f811a564d"
 };
